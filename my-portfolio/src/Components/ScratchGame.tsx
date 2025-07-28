eb1e1da012fb1f840d1d8ac3e3f4a99dbde7ee64
vantaylo <vanessalyn.taylor@gmail.com>
2025-07-28 12:44:32 -0500

    added scratch game component

---
+++ my-portfolio/src/Components/ScratchGame.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const ScratchGame = () => {
+  return (
+    <div>
+      <iframe src="https://scratch.mit.edu/projects/382209881/embed" allowTransparency={true} width="485" height="402" allowFullScreen></iframe>
+    </div>
+  )
+}
+
+export default ScratchGame;
+
